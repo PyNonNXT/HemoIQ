590b4c895714f34cb0ff47f80afd319358e9062b
PyNonNXT <136231646+PyNonNXT@users.noreply.github.com>
2026-08-09 20:28:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -791,7 +791,7 @@ Blood-test data:
 """
 
         conclusion = client.chat.completions.create(
-            model="openai/gpt-oss-20b",
+            model="openai/gpt-oss-20b:free",
             messages=[
                {"role": "user", "content": prompt}
             ]
